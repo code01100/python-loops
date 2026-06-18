5e19d44b6a176b175ed9e8b53098a1d07af3d9ac
code01100 <whisperingcomet23@gmail.com>
2026-06-18 13:45:16 -0400

    last update on loops Module

--- loops.py
+++ loops.py
@@ -57,3 +57,22 @@
 #     print(f"No one is eligible to manage the staff")
 
 
+# using dictionary instead of repeated cases
+users = [
+
+    {"id": 1,"total": 100, "coupon": "P20"},
+    {"id": 2,"total": 70, "coupon": "F40"},
+    {"id": 3,"total": 90, "coupon": "S50"},
+]
+
+discounts ={
+    "P20": (0.2,0),
+    "F40": (0.5,0),
+    "S50": (0,10)
+}
+
+
+for user in users:
+    percent, fixed = discounts.get(user["coupon"], (0.0))
+    discount = user["total"] * percent + fixed
+    print(f"{user["id"]} paid {user["total"]} and got discount for next visit of dollars {discount}")
